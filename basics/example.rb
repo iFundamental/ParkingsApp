c4357079cbc3632a525f8c2357adb02dc57b948e
sally <smclean17@gmail.com>
2015-02-23 23:31:19 +1100

    #7 indentation fix 2

--- basics/example.rb
+++ basics/example.rb
@@ -2,15 +2,15 @@ class Article
   attr_accessor :likes, :dislikes
   attr_reader :title, :body, :author, :created_at
   def initialize(title, body, author=nil)
-    	@title=title
-    	@body=body
-    	@author=author
-    	@created_at=Time.now
-    	@likes=0
-    	@dislikes=0
+    @title=title
+    @body=body
+    @author=author
+    @created_at=Time.now
+    @likes=0
+    @dislikes=0
   end
   def like!
-  		@likes += 1
+  	  @likes += 1
   end
   def dislike!
   	@dislikes +=1
@@ -29,9 +29,9 @@ class Article
   end
   def truncate(limit)
     if @body.length <= limit
-  		@body
+  	  @body
     else
-  		@body[0,limit-3] << "..."
+  	  @body[0,limit-3] << "..."
    end
   end
   def contain?(search_string)

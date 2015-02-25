14f5940b4bb86fc8b33a1e9e1938f42e76193565
Sally Mclean <smclean17@gmail.com>
2015-02-25 19:26:07 +0100

    #31 Create PlaceRent validations and Validation tests

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -1,4 +1,5 @@
 class PlaceRent < ActiveRecord::Base
-	belongs_to :cars
-	belongs_to :parkings
+  belongs_to :cars
+  belongs_to :parkings
+  validates :starts_at, :ends_at, :parkings, :cars, :presence => true
 end
--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should have the necessary required validators" do
+    place_rent = PlaceRent.new
+    assert place_rent.invalid?
+    assert_equal [:starts_at, :ends_at, :parkings, :cars], place_rent.errors.keys
+  end
 end

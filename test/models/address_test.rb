6e3811179e666103c187fafc497a128c6496b252
Sally Mclean <smclean17@gmail.com>
2015-02-25 18:53:37 +0100

    #27 Add tests for Address model validations

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -4,4 +4,22 @@ class AddressTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should have the necessary required validators" do
+    a = Address.new
+    assert_not a.valid?
+    assert_equal [ :city, :street, :zip_code ], a.errors.keys
+  end
+  test "Zip code should have specific format 34-483" do
+    a = Address.new(city: 'Melbourne', street:'Anthony Street', zip_code: 'xxxxx')
+    assert_not a.valid?
+
+    @invalid_values =['2333-23423', '53522', '33-3333', '2t-43e']
+    @invalid_values.each do |value|
+      a.zip_code=value
+      assert_not a.valid?, value + ' should not be a valid value.'
+    end
+
+    a.zip_code='45-245'
+    assert a.valid?, '45-245 should be a valid value.'
+  end
 end

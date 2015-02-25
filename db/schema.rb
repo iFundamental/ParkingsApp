11a860d5a5651dbc745221c47302edf790d1e34e
Sally Mclean <smclean17@gmail.com>
2015-02-25 12:37:51 +0100

    #23 Create Person Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225113320) do
+ActiveRecord::Schema.define(version: 20150225113701) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "city"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150225113320) do
     t.string   "registration_number"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

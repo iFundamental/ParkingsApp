f8507658dbebbb4b7ee03865adeeadd42c1721d2
Sally Mclean <smclean17@gmail.com>
2015-02-25 17:20:43 +0100

    #29 Add validations and validation test to Person Model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -4,4 +4,9 @@ class PersonTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should have the necessary required validators" do
+  post = Person.new
+  assert_not post.valid?
+  assert_equal [:first_name], post.errors.keys
+end
 end

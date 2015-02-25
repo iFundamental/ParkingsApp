958451bb0162bb3376e2e50def769a1f4239f0d5
Sally Mclean <smclean17@gmail.com>
2015-02-25 19:19:45 +0100

    #27 Fix address model tests

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -6,17 +6,17 @@ class AddressTest < ActiveSupport::TestCase
   # end
   test "should have the necessary required validators" do
     a = Address.new
-    assert_not a.valid?
+    assert a.invalid?
     assert_equal [ :city, :street, :zip_code ], a.errors.keys
   end
   test "Zip code should have specific format 34-483" do
     a = Address.new(city: 'Melbourne', street:'Anthony Street', zip_code: 'xxxxx')
-    assert_not a.valid?
+    assert a.invalid?
 
     @invalid_values =['2333-23423', '53522', '33-3333', '2t-43e']
     @invalid_values.each do |value|
       a.zip_code=value
-      assert_not a.valid?, value + ' should not be a valid value.'
+      assert a.invalid?, value + ' should not be a valid value.'
     end
 
     a.zip_code='45-245'

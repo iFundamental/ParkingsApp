dc70545a41498073340955c63efb2e9f1e456ea5
Sally Mclean <smclean17@gmail.com>
2015-02-25 19:01:39 +0100

    #29 Fix person model validation tests

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,12 +1,10 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "should have the necessary required validators" do
-  post = Person.new
-  assert_not post.valid?
-  assert_equal [:first_name], post.errors.keys
-end
+    person = Person.new
+    assert person.invalid?
+    assert_equal [:first_name], person.errors.keys
+  end
 end

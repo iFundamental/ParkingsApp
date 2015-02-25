305411040a294cb1bc5745585dbc1cdfe6007d7b
Sally Mclean <smclean17@gmail.com>
2015-02-25 17:17:27 +0100

    #28 Add validations and validation test to Cars Model

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -4,4 +4,9 @@ class CarTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+ test "should have the necessary required validators" do
+  post = Car.new
+  assert_not post.valid?
+  assert_equal [:model, :registration_number, :owner], post.errors.keys
+end
 end

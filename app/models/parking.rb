60e80d14f1ee205a339aced15029eea11b7fddc4
Sally Mclean <smclean17@gmail.com>
2015-02-25 19:17:02 +0100

    #30 Fix Parking model test

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -5,7 +5,7 @@ class Parking < ActiveRecord::Base
   validates :hour_price, :day_price, :presence => true,  :numericality => true
   validate :kind_should_have_valid_value
 
-   def kind_should_have_valid_value
+  def kind_should_have_valid_value
     if !['outdoor', 'indoor', 'private', 'street'].include? kind
       errors.add(:kind, "kind must be a value of outdoor, indoor, private or street")
     end

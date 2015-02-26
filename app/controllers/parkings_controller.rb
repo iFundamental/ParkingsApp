0ab83fe97de558f8b828c05386d26d477c2f76db
Sally Mclean <smclean17@gmail.com>
2015-02-26 15:59:32 +0100

    #35 Add edit and update controllers to parking

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,30 +1,42 @@
 class ParkingsController < ApplicationController
   def index
-  	@parkings = Parking.all
+    @parkings = Parking.all
   end
 
   def show
-  	@parking = Parking.find(params[:id])
-
+    @parking = Parking.find(params[:id])
   end
 
   def new
-  	@parking = Parking.new
+    @parking = Parking.new
   end
 
   def create
-  	@parking = Parking.new(parking_params)
-  	if @parking.save
-    redirect_to @parking
-  else
-    render 'new'
-  end
-  	
+    @parking = Parking.new(parking_params)
+    if @parking.save
+      redirect_to @parking
+    else
+      render 'new'
+    end
   end
 
   private
 
   def parking_params
     params.require(:parking).permit(:kind, :places, :hour_price, :day_price, :text)
+  end
+
+  def edit
+    @parking = Parking.find(params[:id])
+  end
+
+  def update
+    @parking = Parking.find(params[:id])
+    @parking.update(parking_params)
+    if @parking.save
+      redirect_to @parking
+    else
+      render 'new'
+    end
   end
 end

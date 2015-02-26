311a39219d888bfb455565facf19057486a38018
Sally Mclean <smclean17@gmail.com>
2015-02-26 10:31:33 +0100

    #27 Remove assertians from loop for address tests

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,23 +1,26 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "should have the necessary required validators" do
     a = Address.new
     assert a.invalid?
     assert_equal [ :city, :street, :zip_code ], a.errors.keys
   end
+
   test "Zip code should have specific format 34-483" do
     a = Address.new(city: 'Melbourne', street:'Anthony Street', zip_code: 'xxxxx')
-    assert a.invalid?
+    
+    a.zip_code='2333-23423'
+    assert a.invalid?, '2333-23423 should not be a valid value.'
 
-    @invalid_values =['2333-23423', '53522', '33-3333', '2t-43e']
-    @invalid_values.each do |value|
-      a.zip_code=value
-      assert a.invalid?, value + ' should not be a valid value.'
-    end
+    a.zip_code='53522'
+    assert a.invalid?, '53522 should not be a valid value.'
+
+    a.zip_code='33-3333'
+    assert a.invalid?, '33-3333 should not be a valid value.'
+
+    a.zip_code='2t-43e'
+    assert a.invalid?, '2t-43e should not be a valid value.'
 
     a.zip_code='45-245'
     assert a.valid?, '45-245 should be a valid value.'

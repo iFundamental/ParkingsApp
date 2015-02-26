36784abe8f708321fb25ccd137e55b45c35e8be3
Sally Mclean <smclean17@gmail.com>
2015-02-26 10:05:50 +0100

    #30 Change Parking Kind validation to use builtin inclusion validation

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,12 +2,7 @@ class Parking < ActiveRecord::Base
   belongs_to :owner, :class_name => 'Person'
   belongs_to :address
   has_many :place_rents
-  validates :hour_price, :day_price, :presence => true,  :numericality => true
-  validate :kind_should_have_valid_value
 
-  def kind_should_have_valid_value
-    if !['outdoor', 'indoor', 'private', 'street'].include? kind
-      errors.add(:kind, "kind must be a value of outdoor, indoor, private or street")
-    end
-  end
+  validates :hour_price, :day_price, :presence => true,  :numericality => true
+  validates :kind, inclusion: { in: %w(outdoor indoor private street), message: "%{value} is not a valid kind" }
 end

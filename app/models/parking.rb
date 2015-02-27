3eb0f05440cdfb3873330b712ea8d5fb40bb323e
Sally Mclean <smclean17@gmail.com>
2015-02-27 16:51:18 +0100

    #38 change :true to true for model settings

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -3,8 +3,8 @@ class Parking < ActiveRecord::Base
   belongs_to :address, dependent: :destroy
   has_many :place_rents
 
-  validates :hour_price, :day_price, presence: :true,  numericality: :true
+  validates :hour_price, :day_price, presence: true,  numericality: true
   validates :kind, inclusion: { in: %w(outdoor indoor private street), message: "%{value} is not a valid kind" }
 
-  accepts_nested_attributes_for :address, allow_destroy: :true
+  accepts_nested_attributes_for :address, allow_destroy: true
 end
--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -1,5 +1,5 @@
 class PlaceRent < ActiveRecord::Base
   belongs_to :car
   belongs_to :parking
-  validates :starts_at, :ends_at, :parking, :car, presence: :true
+  validates :starts_at, :ends_at, :parking, :car, presence: true
 end

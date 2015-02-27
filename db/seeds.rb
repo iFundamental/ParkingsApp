3f5b0e343298e39412eb72cdbc433fc61c92d772
Sally Mclean <smclean17@gmail.com>
2015-02-27 10:16:52 +0100

    #39 Add person using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  person = Person.create([{ first_name: 'Sally', last_name: 'Mclean' }])
+  

936d9854f9f49b016b305d895e6687997110de51
Sally Mclean <smclean17@gmail.com>
2015-03-01 14:03:59 +0100

    #42 Added car_model_registration to better identify a car

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,11 @@ class Car < ActiveRecord::Base
   belongs_to :owner, class_name: :'Person'
   has_many :place_rents
   validates :model, :registration_number, :owner, presence: true
+
+
+
+
+  def car_model_registration
+    model + ' - ' + registration_number
+  end
 end

7d0f82fc8186f5b01a5ee829cee6c6404d718b78
Sally Mclean <smclean17@gmail.com>
2015-03-02 01:34:36 +0100

    #46 capybara config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  class ActionDispatch::IntegrationTest
-    # Make the Capybara DSL available in all integration tests
-    include Capybara::DSL
-  end
+  
+end
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
 end

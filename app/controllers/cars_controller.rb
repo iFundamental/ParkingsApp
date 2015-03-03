9db52d578d14fe8468628c102c9b6ce214066ac0
Sally Mclean <smclean17@gmail.com>
2015-03-03 12:03:44 +0100

    #40 remove set_car method comments and json format

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,28 +1,21 @@
 class CarsController < ApplicationController
-  before_action :set_car, only: [:show, :edit, :update, :destroy]
 
-  # GET /cars
-  # GET /cars.json
   def index
     @cars = current_person.cars.all
   end
 
-  # GET /cars/1
-  # GET /cars/1.json
   def show
+    @car = Car.find(params[:id])
   end
 
-  # GET /cars/new
   def new
     @car = current_person.cars.new
   end
 
-  # GET /cars/1/edit
   def edit
+    @car = Car.find(params[:id])
   end
 
-  # POST /cars
-  # POST /cars.json
   def create
     @car = current_person.cars.new(car_params)
     if @car.save
@@ -32,8 +25,6 @@ class CarsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cars/1
-  # PATCH/PUT /cars/1.json
   def update
     if @car.update(car_params)
       redirect_to @car, notice: 'Car was successfully updated.'
@@ -42,8 +33,6 @@ class CarsController < ApplicationController
     end
   end
 
-  # DELETE /cars/1
-  # DELETE /cars/1.json
   def destroy
     @car.destroy
     redirect_to cars_url, notice: 'Car was successfully deleted.'
@@ -51,13 +40,6 @@ class CarsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-
-  def set_car
-    @car = Car.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
   def car_params
     params.require(:car).permit(:model, :registration_number)
   end

3c8e3494ee67472275e22b6c1174831bd9dc9689
Sally Mclean <smclean17@gmail.com>
2015-03-04 13:32:04 +0100

    #51 Add logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,31 @@
 class SessionsController < ApplicationController
 
   def new
-    #session[:account_id] = ''
     @account = Account.new
   end
 
   def create
-    
     @account = Account.find_by email: session_params[:email]
     if @account.nil?
       log_in_failed
+    elsif @account.password == session_params[:password]
+      session[:account_id] =  @account.id
+      redirect_to parkings_url
     else
-      if @account.password == session_params[:password]
-        session[:account_id] =  @account.id
-        redirect_to parkings_url
-      else
-        log_in_failed
-      end
+      log_in_failed
     end
+  end
+
+  def destroy
+    session[:account_id] = ''
+    redirect_to root_url
   end
 
   private
 
   def log_in_failed
     redirect_to new_session_url, notice: 'Login failed. Incorrect username or password.'
-
   end
-
 
   def session_params
     params.require(:account).permit(:email, :password)

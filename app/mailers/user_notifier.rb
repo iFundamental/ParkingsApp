4b8920bb1b67ab80b1a8f81399328fa8e4f93c92
Sally Mclean <smclean17@gmail.com>
2015-03-06 12:14:39 +0100

    #89 Add login with facebook link

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,7 +1,6 @@
 class UserNotifier < ApplicationMailer
   def send_signup_email(account)
     @account=account
-     mail(to: account.email, subject: 'Welcome to Bookparking',
-       template_name: 'send_signup_email')
+     mail(to: account.email, subject: 'Welcome to Bookparking', template_name: 'send_signup_email')
   end
 end

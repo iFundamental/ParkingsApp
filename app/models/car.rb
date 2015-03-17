f0d15c44c1c167e13874d4206e3146a43a1eb008
Sally Mclean <smclean17@gmail.com>
2015-03-18 06:25:13 +1100

    #84 Add image validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,8 @@ class Car < ActiveRecord::Base
   has_many :place_rents, dependent: :destroy
   validates :model, :registration_number, :owner, presence: true
   dragonfly_accessor :image
-  validates_property :size, of: :image, in: (0..200), message: 'The image should not be greater than 200 KB'
+  validates_size_of :image, maximum: 200.kilobytes, message:'should not be greater than 200 KB', if: :image_changed?
+  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false, message: 'must be an image file format of .jpeg, .jpg, .png, .bmp', if: :image_changed?
 
   def car_model_registration
     model + ' - ' + registration_number

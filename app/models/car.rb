cce0e64c82d9cea230928be3644c8b5075e01278
Sally Mclean <smclean17@gmail.com>
2015-03-18 05:52:24 +1100

    #84 Add image size validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ActiveRecord::Base
   has_many :place_rents, dependent: :destroy
   validates :model, :registration_number, :owner, presence: true
   dragonfly_accessor :image
+  validates_property :size, of: :image, in: (0..200), message: 'The image should not be greater than 200 KB'
 
   def car_model_registration
     model + ' - ' + registration_number
--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,11 +3,10 @@ require 'dragonfly'
 # Configure
 Dragonfly.app.configure do
   plugin :imagemagick
-
   secret "1a42a77e575c285d392b3c2b5a5d6bb601e34c73a49bad0c6773fe48e1389089"
 
   url_format "/media/:job/:name"
-
+  convert_command = "/usr/local/bin/convert"
   datastore :file,
     root_path: Rails.root.join('public/system/dragonfly', Rails.env),
     server_root: Rails.root.join('public')

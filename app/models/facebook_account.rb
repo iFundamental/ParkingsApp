d4e2024215e1128a4415214952fc510f01b0ecd9
Sally Mclean <smclean17@gmail.com>
2015-03-20 07:27:22 +1100

    #90 Change facebook login to match existing user if email exists intead of creating a new person

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -6,7 +6,12 @@ class FacebookAccount < ActiveRecord::Base
   def self.find_or_create_for_facebook(auth_hash)
     facebook_account = find_by_uid(auth_hash['uid'])
     if facebook_account.nil?
-      person = Person.create(first_name: auth_hash['info']['first_name'], last_name: auth_hash['info']['last_name'])
+      account = Account.find_by_email(auth_hash['info']['email'])
+      if account
+        person = account.person
+      else
+        person = Person.create(first_name: auth_hash['info']['first_name'], last_name: auth_hash['info']['last_name'])
+      end
       facebook_account = FacebookAccount.create(person: person, uid: auth_hash['uid'])
     end
     facebook_account
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 
   Person.delete_all
   Account.delete_all
+  FacebookAccount.delete_all
 
   person = Person.create(first_name: 'Sally', last_name: 'Mclean')
   Account.create(email: 'smclean17@gmail.com', password_digest:  BCrypt::Password.create('testpassword'), person: person)
